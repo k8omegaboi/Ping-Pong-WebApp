8b339b513347f6cd1b2bf4b30b9aef2a11b3fae5
AJ-DJ <94147359+k8omegaboi@users.noreply.github.com>
2022-04-12 17:24:54 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ function setup()
     canvas = createCanvas(600,300);
     video = createCapture(VIDEO);
     canvas.center();
-    video.hide()
-    parent(canvas,"canvas");
+    video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded()
@@ -23,7 +23,8 @@ function gotPoses(results)
     {
         rwristx = result[0].pose.rightWrist.x;
         rwristy = result[0].pose.rightWrist.y;
-        rwristscore = results[0].pose.keypoints[10].score        
+        rwristscore = results[0].pose.keypoints[10].score;
+        console.log(results);
     }
 }
 
@@ -35,6 +36,6 @@ function draw()
     {
         fill("#ff0000");
         stroke("#ff0000");
-        circle(rwristx, rwristy, 20)
+        circle(rwristx, rwristy, 20);
     }
 }
